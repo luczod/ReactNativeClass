1d1f4bd1ad376e76df339bd0a85f00d02c94db65
luczod <kzod5611@gmail.com>
2023-10-01 16:20:14 -0300

    initial

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <>
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-red-500 font-semibold">Hello World</Text>
+        <Text> mais uma tentativa</Text>
+      </View>
+    </>
+  );
+}
